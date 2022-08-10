b1d40b567821c1f6ed4d0ba9720307be5f5d42dd
chismoc <chisomo.chingwalu@yahoo.co.uk>
2022-08-10 18:16:15 +0200

    swipe to refresh working

--- app/src/main/java/com/example/android/imcnewsapp/MainActivity.java
+++ app/src/main/java/com/example/android/imcnewsapp/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.android.imcnewsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.NestedScrollView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.GridView;
+import android.widget.ProgressBar;
 
 import com.bumptech.glide.request.RequestOptions;
 import com.example.android.imcnewsapp.adapters.GridCategoryAdapter;
@@ -26,7 +31,7 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
     //images for user at homepage
     SliderLayout sliderLayout;
@@ -42,6 +47,18 @@ public class MainActivity extends AppCompatActivity {
     //Categories List
     List<HomepageModel.CategoryButton> categoryButtons;
 
+    // Variables for making infinite news feed
+    int posts = 3;
+    int page = 1;
+    boolean isFromStart = true;
+
+    // Progressbar
+    ProgressBar progressBar;
+
+    NestedScrollView nestedScrollView;
+
+    // Swipe to refresh
+    SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,16 +70,35 @@ public class MainActivity extends AppCompatActivity {
 
         AddImagesToSlider();
 
+        //Initial conditions page 1 as the start
+        page = 1;
+        isFromStart = true;
+
         //Getting news
         getHomeData();
+
+        //Listener for Getting new data on scrolling and swiping down
+        nestedScrollView.setOnScrollChangeListener(new NestedScrollView.OnScrollChangeListener() {
+            @Override
+            public void onScrollChange(NestedScrollView v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+
+                if (scrollY == (v.getChildAt(0).getMeasuredHeight() - v.getMeasuredHeight())){
+                    isFromStart = false;
+                    progressBar.setVisibility(View.VISIBLE);
+                    page++;
+                    getHomeData();
+                }
+            }
+        });
+
     }
 
     private void getHomeData() {
 
-        ApiInterface  apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
+        ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
         Map<String, String> params = new HashMap<>();
-        params.put("page",1+"");
-        params.put("posts",10+"");
+        params.put("page", page + "");
+        params.put("posts", posts + "");
 
         Call<HomepageModel> call = apiInterface.getHomepageApi(params);
         call.enqueue(new Callback<HomepageModel>() {
@@ -76,6 +112,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onFailure(Call<HomepageModel> call, Throwable t) {
 
+                progressBar.setVisibility(View.GONE);
+                swipeRefreshLayout.setRefreshing(false);
             }
         });
     }
@@ -89,7 +127,16 @@ public class MainActivity extends AppCompatActivity {
         // So, our emulator will not get the images
         // we need to replace localhost with the emulator local host 10.0.2.2
 
-        for (int i= 0; i < body.getBanners().size() ; i++) {
+        progressBar.setVisibility(View.GONE);
+        swipeRefreshLayout.setRefreshing(false);
+
+        if (isFromStart) {
+            news.clear();
+            categoryButtons.clear();
+        }
+
+
+        for (int i = 0; i < body.getBanners().size(); i++) {
             DefaultSliderView defaultSliderView = new DefaultSliderView(this);
             defaultSliderView.setRequestOption(new RequestOptions().centerCrop());
             defaultSliderView.image(body.getBanners().get(i).getImage());
@@ -111,27 +158,42 @@ public class MainActivity extends AppCompatActivity {
         sliderLayout.setPresetIndicator(SliderLayout.PresetIndicators.Right_Bottom);
 
 
+        // Making way for getting news in correct order and conserving the page numbers
+        int beforeNewsSize = news.size();
 
-        for(int i = 0; i < body.getNews().size(); i++){
+
+        for (int i = 0; i < body.getNews().size(); i++) {
             news.add(body.getNews().get(i));
         }
+        categoryButtons.addAll(body.getCategoryButton());
 
-        recyclerView.setAdapter(newsAdapter);
+        if (isFromStart) {
+            recyclerView.setAdapter(newsAdapter);
+            gridView.setAdapter(gridCategoryAdapter);
+        }else{
+            newsAdapter.notifyItemRangeInserted(beforeNewsSize, body.getNews().size());
+        }
 
-categoryButtons.addAll(body.getCategoryButton());
-        gridView.setAdapter(gridCategoryAdapter);
+
     }
 
     private void AddImagesToSlider() {
 
     }
 
+    @SuppressLint("ResourceAsColor")
     private void InitialiseViews() {
 
         categoryButtons = new ArrayList<>();
         sliderLayout = findViewById(R.id.slider);
         gridView = findViewById(R.id.gridView);
         gridCategoryAdapter = new GridCategoryAdapter(this, categoryButtons);
+
+        //progress bar
+        progressBar = findViewById(R.id.progressBar);
+
+        // Nested scrollview
+        nestedScrollView = findViewById(R.id.nestedView);
 
 
         // RecyclerView
@@ -143,6 +205,16 @@ categoryButtons.addAll(body.getCategoryButton());
         news = new ArrayList<>();
         newsAdapter = new NewsAdapter(this, news);
 
+        swipeRefreshLayout = findViewById(R.id.swipeLayout);
+        swipeRefreshLayout.setRefreshing(true);
+        swipeRefreshLayout.setColorSchemeColors(R.color.purple_200,
+                R.color.purple_500,
+                R.color.purple_700);
+
+
+        swipeRefreshLayout.setOnRefreshListener(this);
+
+
     }
 
     //if slider is stopped close the slider
@@ -152,4 +224,11 @@ categoryButtons.addAll(body.getCategoryButton());
         sliderLayout.stopAutoCycle();
     }
 
+    @Override
+    public void onRefresh() {
+        isFromStart = true;
+        page = 1;
+        getHomeData();
+
+    }
 }
