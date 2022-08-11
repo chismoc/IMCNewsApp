351a28bc74875092c6255da7a75512331e556e35
chismoc <chisomo.chingwalu@yahoo.co.uk>
2022-08-11 12:35:51 +0200

    News Details Activity Created

---
+++ app/src/main/java/com/example/android/imcnewsapp/activities/NewsDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.android.imcnewsapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.android.imcnewsapp.R;
+
+public class NewsDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_details);
+    }
+}
